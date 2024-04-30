db0f24a62c315c925f8f858b0ff750a2f915e5af
ebcengiz <enesbugracengiz@gmail.com>
2024-04-30 16:52:44 +0300

    main title component created

--- ReactDers4/src/components/card/Card.jsx
+++ ReactDers4/src/components/card/Card.jsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import "./card.css"
+import MainTitle from '../mainTitle/MainTitle'
 
 const Card = () => {
   return (
     <>
         <div className='card'>
-            
+            <MainTitle text="Filter by author" />
         </div>
     </>
   )
--- ReactDers4/src/components/mainTitle/MainTitle.jsx
+++ ReactDers4/src/components/mainTitle/MainTitle.jsx
@@ -1,10 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import "./mainTitle.css"
 
-const MainTitle = () => {
+// eslint-disable-next-line react/prop-types
+const MainTitle = ({text}) => {
   return (
     <>
-      
+      <div className="main-title-box">
+        <p>{text}</p>
+      </div>
     </>
   )
 }
