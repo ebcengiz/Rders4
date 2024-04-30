ed75c203e4332fe85f7bedcdc2b6794c6c75667e
ebcengiz <enesbugracengiz@gmail.com>
2024-04-30 17:24:40 +0300

    search bar component created

--- ReactDers4/src/components/usersCard/UsersCard.jsx
+++ ReactDers4/src/components/usersCard/UsersCard.jsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import "./usersCard.css"
+import SearchBar from '../searchBar/SearchBar'
 
 const UsersCard = () => {
   return (
     <>
         <div className='users-card-box'>
-            
+            <SearchBar />
         </div>
     </>
   )
